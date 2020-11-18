a45276db3f0b82f5947814f95c8520027d1892e5
Andres Medrano <amedra13@gmail.com>
2020-11-17 17:53:12 -0800

    finished module

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,7 +28,13 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
 	res.render('shop/cart', {
 		path: '/cart',
-		pageTitle: 'your Cart',
+		pageTitle: 'Your Cart',
+	});
+};
+exports.getOrders = (req, res, next) => {
+	res.render('shop/orders', {
+		path: '/orders',
+		pageTitle: 'Your Orders',
 	});
 };
 

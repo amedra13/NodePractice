fb218055e083836329657688626cfdd9bd31b4d7
Andres Medrano <amedra13@gmail.com>
2020-11-19 18:47:56 -0800

    fix deleting from cart

--- models/cart.js
+++ models/cart.js
@@ -9,14 +9,19 @@ const p = path.join(
 
 module.exports = class Cart {
 	static addProduct(id, productPrice) {
+		// Fetch the previous cart
 		fs.readFile(p, (err, fileContent) => {
 			let cart = { products: [], totalPrice: 0 };
 			if (!err) {
 				cart = JSON.parse(fileContent);
 			}
-			const existingProductIndex = cart.products.findIndex((p) => p.id === id);
+			// Analyze the cart => Find existing product
+			const existingProductIndex = cart.products.findIndex(
+				(prod) => prod.id === id
+			);
 			const existingProduct = cart.products[existingProductIndex];
 			let updatedProduct;
+			// Add new product/ increase quantity
 			if (existingProduct) {
 				updatedProduct = { ...existingProduct };
 				updatedProduct.qty = updatedProduct.qty + 1;
@@ -35,20 +40,32 @@ module.exports = class Cart {
 
 	static deleteProduct(id, productPrice) {
 		fs.readFile(p, (err, fileContent) => {
-			// let cart = { products: [], totalPrice: 0 };
 			if (err) {
 				return;
 			}
-
 			const updatedCart = { ...JSON.parse(fileContent) };
-			const product = updatedCart.products.findIndex((prod) => prod.id === id);
+			const product = updatedCart.products.find((prod) => prod.id === id);
 			const productQty = product.qty;
-			updatedCart.products = updatedCart.products.filter((p) => prod.id !== id);
+			updatedCart.products = updatedCart.products.filter(
+				(prod) => prod.id !== id
+			);
 			updatedCart.totalPrice =
 				updatedCart.totalPrice - productPrice * productQty;
+
 			fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
 				console.log(err);
 			});
+		});
+	}
+
+	static getCart(cb) {
+		fs.readFile(p, (err, fileContent) => {
+			const cart = JSON.parse(fileContent);
+			if (err) {
+				cb(null);
+			} else {
+				cb(cart);
+			}
 		});
 	}
 };

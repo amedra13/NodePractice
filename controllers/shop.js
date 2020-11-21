f89d2ff35acc088d90408d9d4facdb2c3e3059f8
Andres Medrano <amedra13@gmail.com>
2020-11-20 19:52:39 -0800

    connect sql database to project

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,16 +3,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll((products) => {
-		res.render('shop/product-list', {
-			prods: products,
-			pageTitle: 'All Products',
-			path: '/products',
-			hasProducts: products.length > 0,
-			activeShop: true,
-			productCSS: true,
+	Product.fetchAll()
+		.then(([rows]) => {
+			res.render('shop/product-list', {
+				prods: rows,
+				pageTitle: 'All Products',
+				path: '/products',
+			});
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-	});
 };
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
@@ -26,16 +27,18 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-	Product.fetchAll((products) => {
-		res.render('shop/index', {
-			prods: products,
-			pageTitle: 'Shop',
-			path: '/',
-			hasProducts: products.length > 0,
-			activeShop: true,
-			productCSS: true,
+	Product.fetchAll()
+		.then(([rows]) => {
+			console.log(rows);
+			res.render('shop/index', {
+				prods: rows,
+				pageTitle: 'Shop',
+				path: '/',
+			});
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-	});
 };
 exports.getCart = (req, res, next) => {
 	Cart.getCart((cart) => {

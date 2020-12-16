bcd1871a24ff7dade5087ba3ed265523977db963
Andres Medrano <amedra13@gmail.com>
2020-12-15 18:46:12 -0800

    install sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,19 +17,20 @@ exports.getProducts = (req, res, next) => {
 };
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
-	findById(prodId, (product) => {
-		res.render('shop/product-detail', {
-			product: product,
-			pageTitle: product.title,
-			path: '/products',
-		});
-	});
+	Product.findById(prodId)
+		.then(([product]) => {
+			res.render('shop/product-detail', {
+				product: product[0],
+				pageTitle: product.title,
+				path: '/products',
+			});
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
 	Product.fetchAll()
 		.then(([rows]) => {
-			console.log(rows);
 			res.render('shop/index', {
 				prods: rows,
 				pageTitle: 'Shop',
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'Omfk7710!', {
+	dialect: 'mysql',
 	host: 'localhost',
-	user: 'root',
-	database: 'node-complete',
-	password: 'Omfk7710!',
 });
 
-module.exports = pool.promise();
+module.export = sequelize;

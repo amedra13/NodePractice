e5bba9e9ca090d0c207b6d0df6ff862e39bdbd55
Andres Medrano <amedra13@gmail.com>
2020-12-27 14:41:07 -0800

    fixed bug with get product detail

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,10 +14,10 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
 	Product.findById(prodId)
-		.then((products) => {
+		.then((product) => {
 			res.render('shop/product-detail', {
-				product: products[0],
-				pageTitle: products[0].title,
+				product: product,
+				pageTitle: product.title,
 				path: '/products',
 			});
 		})

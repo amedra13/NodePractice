d6b14db3524b3e01724a1005a07018a5161f230a
Andres Medrano <amedra13@gmail.com>
2021-01-14 15:11:28 -0800

    able to post to cart and delete from cart

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-// router.post('/cart-delete-item', shopController.postCartProductDelete);
+router.post('/cart-delete-item', shopController.postCartProductDelete);
 // router.get('/orders', shopController.getOrders);
 // router.get('/checkout', shopController.getCheckout);
 // router.post('/create-order', shopController.postOrder);

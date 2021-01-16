37d8f4014ea6afc3fd371343cd88a545e02f2405
Andres Medrano <amedra13@gmail.com>
2021-01-15 18:45:42 -0800

    begin converting logic using mongoose

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -16,20 +16,27 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-	User.findById('5febdb67c805dd6c14548fcb')
-		.then((user) => {
-			req.user = new User(user.name, user.email, user.cart, user._id);
-			next();
-		})
-		.catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+// 	User.findById('5febdb67c805dd6c14548fcb')
+// 		.then((user) => {
+// 			req.user = new User(user.name, user.email, user.cart, user._id);
+// 			next();
+// 		})
+// 		.catch((err) => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-	app.listen(3000);
-});
+mongoose
+	.connect(
+		'mongodb+srv://Andres:8Wijs9lpBV2HtrM9@cluster0.afspz.mongodb.net/shop'
+	)
+	.then((result) => {
+		app.listen(3000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
--- routes/shop.js
+++ routes/shop.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
-router.get('/products/:productId', shopController.getProduct);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/cart-delete-item', shopController.postCartProductDelete);
-router.get('/orders', shopController.getOrders);
+// router.get('/products/:productId', shopController.getProduct);
+// router.get('/cart', shopController.getCart);
+// router.post('/cart', shopController.postCart);
+// router.post('/cart-delete-item', shopController.postCartProductDelete);
+// router.get('/orders', shopController.getOrders);
 // router.get('/checkout', shopController.getCheckout);
-router.post('/create-order', shopController.postOrder);
+// router.post('/create-order', shopController.postOrder);
 
 module.exports = router;

8816773bfbef18274f0358a18b6b88b3306dbb1d
Andres Medrano <amedra13@gmail.com>
2021-01-16 17:43:19 -0800

    edit orders modal in controller and views

--- controllers/shop.js
+++ controllers/shop.js
@@ -105,8 +105,7 @@ exports.postOrder = (req, res, next) => {
 		.catch((err) => console.log(err));
 };
 exports.getOrders = (req, res, next) => {
-	req.user
-		.getOrders()
+	Order.find({ 'user.userId': req.user._id })
 		.then((orders) => {
 			res.render('shop/orders', {
 				path: '/orders',

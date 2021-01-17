48898c357f7397a177888bd3847daef82e09e447
Andres Medrano <amedra13@gmail.com>
2021-01-16 16:19:07 -0800

    user schema includes addToCart function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -16,14 +16,14 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-// 	User.findById('5febdb67c805dd6c14548fcb')
-// 		.then((user) => {
-// 			req.user = new User(user.name, user.email, user.cart, user._id);
-// 			next();
-// 		})
-// 		.catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+	User.findById('6003794b05fec52b2c9961fa')
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,19 @@ mongoose
 		'mongodb+srv://Andres:8Wijs9lpBV2HtrM9@cluster0.afspz.mongodb.net/shop'
 	)
 	.then((result) => {
+		User.findOne().then((user) => {
+			if (!user) {
+				const user = new User({
+					name: 'Andres',
+					email: 'andres@test.com',
+					cart: {
+						items: [],
+					},
+				});
+				user.save();
+			}
+		});
+
 		app.listen(3000);
 	})
 	.catch((err) => {

4d6835b7730931a757c8fc74f1d49873e7ac0cc3
Andres Medrano <amedra13@gmail.com>
2021-01-17 19:23:11 -0800

    begin added auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth')
+const authController = require('../controllers/auth');
 
-router.get('/login', authController.getLogin)
-module.exports = router
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+module.exports = router;

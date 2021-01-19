85fd3bf290dcd1ff154c100ee2136c911ecb21a3
Andres Medrano <amedra13@gmail.com>
2021-01-18 17:55:41 -0800

    impliment a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,12 @@
 exports.getLogin = (req, res, next) => {
+	const loggedIn = req.get('Cookie').split('=')[1];
 	res.render('auth/login', {
 		path: '/login',
 		pageTitle: 'Login',
+		isAuthenticated: loggedIn,
 	});
 };
 exports.postLogin = (req, res, next) => {
-	req.isLoggedIn = true;
+	res.setHeader('Set-Cookie', 'loggedIn=true');
 	res.redirect('/');
 };

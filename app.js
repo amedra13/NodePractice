477fd1f8e9e6c91c94bb43b84fa1aef71c75a68c
Andres Medrano <amedra13@gmail.com>
2021-01-20 17:56:49 -0800

    add csrf protection to views

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongodbStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -17,6 +18,7 @@ const store = new MongodbStore({
 	uri: MONGODB_URL,
 	collection: 'sessions',
 });
+const csrfProtection = csrf();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -34,6 +36,7 @@ app.use(
 		store: store,
 	})
 );
+app.use(csrfProtection);
 app.use((req, res, next) => {
 	if (!req.session.user) {
 		return next();
@@ -44,6 +47,12 @@ app.use((req, res, next) => {
 			next();
 		})
 		.catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);

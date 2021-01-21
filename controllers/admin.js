27e04866b47deb72ea24e499430baf103cc14540
Andres Medrano <amedra13@gmail.com>
2021-01-20 17:18:09 -0800

    protected routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -69,8 +69,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
-		.select('title price -_id')
-		.populate('userId', 'name')
+		// .select('title price -_id')
+		// .populate('userId')
 		.then((products) => {
 			console.log(products);
 			res.render('admin/products', {
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
+
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/cart-delete-item', shopController.postCartProductDelete);
-router.get('/orders', shopController.getOrders);
+router.get('/cart', isAuth, shopController.getCart);
+router.post('/cart', isAuth, shopController.postCart);
+router.post('/cart-delete-item', isAuth, shopController.postCartProductDelete);
+router.get('/orders', isAuth, shopController.getOrders);
 // router.get('/checkout', shopController.getCheckout);
-router.post('/create-order', shopController.postOrder);
+router.post('/create-order', isAuth, shopController.postOrder);
 
 module.exports = router;

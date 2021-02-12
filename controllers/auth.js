616a5ddc378b0e77453162c9e35c45f715aa0ebb
Andres Medrano <amedra13@gmail.com>
2021-02-11 18:17:37 -0800

    validate input and keep data from login/signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,6 +22,11 @@ exports.getLogin = (req, res, next) => {
 		pageTitle: 'Login',
 		isAuthenticated: false,
 		errorMessage: message,
+		oldInput: {
+			email: '',
+			password: '',
+		},
+		validationErrors: [],
 	});
 };
 
@@ -33,6 +38,12 @@ exports.getSignup = (req, res, next) => {
 		pageTitle: 'Signup',
 		isAuthenticated: false,
 		errorMessage: message,
+		oldInput: {
+			email: '',
+			password: '',
+			confirmPassword: '',
+		},
+		validationErrors: [],
 	});
 };
 
@@ -47,14 +58,28 @@ exports.postLogin = (req, res, next) => {
 			pageTitle: 'Signup',
 			isAuthenticated: false,
 			errorMessage: errors.array()[0].msg,
+			oldInput: {
+				email: email,
+				password: password,
+			},
+			validationErrors: errors.array(),
 		});
 	}
 
 	User.findOne({ email: email })
 		.then((user) => {
 			if (!user) {
-				req.flash('error', 'Invalid email or password');
-				return res.redirect('/login');
+				return res.status(422).render('auth/login', {
+					path: '/signup',
+					pageTitle: 'Signup',
+					isAuthenticated: false,
+					errorMessage: 'Invalid email or password',
+					oldInput: {
+						email: email,
+						password: password,
+					},
+					validationErrors: [],
+				});
 			}
 			bcrypt.compare(password, user.password).then((doMatch) => {
 				if (doMatch) {
@@ -65,15 +90,24 @@ exports.postLogin = (req, res, next) => {
 						res.redirect('/');
 					});
 				}
-				req.flash('error', 'Invalid email or password');
-				res.redirect('/login');
+				return res.status(422).render('auth/login', {
+					path: '/signup',
+					pageTitle: 'Signup',
+					isAuthenticated: false,
+					errorMessage: 'Invalid email or password',
+					oldInput: {
+						email: email,
+						password: password,
+					},
+					validationErrors: [],
+				});
 			});
 		})
 		.catch((err) => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
-	const { email, password } = req.body;
+	const { email, password, confirmPassword } = req.body;
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		console.log(errors.array());
@@ -82,6 +116,12 @@ exports.postSignup = (req, res, next) => {
 			pageTitle: 'Signup',
 			isAuthenticated: false,
 			errorMessage: errors.array()[0].msg,
+			oldInput: {
+				email: email,
+				password: password,
+				confirmPassword: confirmPassword,
+			},
+			validationErrors: errors.array(),
 		});
 	}
 	bcrypt

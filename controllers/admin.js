14bdb26a7fdf537a29065c4434300c2b9a7cee6a
Andres Medrano <amedra13@gmail.com>
2021-02-17 21:18:14 -0800

    update DOM using delete func asychronously

--- controllers/admin.js
+++ controllers/admin.js
@@ -165,8 +165,8 @@ exports.getProducts = (req, res, next) => {
 		});
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-	const { productId } = req.body;
+exports.deleteProduct = (req, res, next) => {
+	const { productId } = req.params;
 	Product.findById(productId)
 		.then((product) => {
 			if (!product) {
@@ -177,11 +177,9 @@ exports.postDeleteProduct = (req, res, next) => {
 		})
 		.then((result) => {
 			console.log('PRODUCT WAS DELETED');
-			res.redirect('/admin/products');
+			res.status(200).json({ message: 'Success!' });
 		})
 		.catch((err) => {
-			const error = new Error(err);
-			error.httpStatusCode = 500;
-			return next(error);
+			res.status(500).json({ message: 'Deleting product Failed!' });
 		});
 };

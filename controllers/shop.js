8e39641b817d215b924e9bd050dd5adf4ad02d0a
Andres Medrano <amedra13@gmail.com>
2021-02-21 15:52:55 -0800

    begin implementing checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -134,12 +134,29 @@ exports.postCartProductDelete = (req, res, next) => {
 			return next(error);
 		});
 };
-// exports.getCheckout = (req, res, next) => {
-// 	res.render('shop/checkout', {
-// 		path: '/checkout',
-// 		pageTitle: 'Checkout',
-// 	});
-// };
+exports.getCheckout = (req, res, next) => {
+	req.user
+		.populate('cart.items.productId')
+		.execPopulate()
+		.then((user) => {
+			const products = user.cart.items;
+			let total = 0;
+			products.forEach((p) => {
+				total += p.quantity * p.productId.price;
+			});
+			res.render('shop/checkout', {
+				path: '/checkout',
+				pageTitle: 'Checkout',
+				products: products,
+				totalSum: total,
+			});
+		})
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
+};
 exports.postOrder = (req, res, next) => {
 	const productItems = req.user
 		.populate('cart.items.productId')
